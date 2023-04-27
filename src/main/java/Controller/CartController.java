4f3321601d68079659bcc05f1a70894ee71a9e95
tobiasbkreamer <tobias@kraemerne.dk>
2023-04-27 09:56:04 +0200

    Update 1.0

--- src/main/java/Controller/CartController.java
+++ src/main/java/Controller/CartController.java
@@ -12,61 +12,47 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class CartController {
 
-    private Cart cart; // reference til indkøbskurv session
+    private Cart cart;
+
 
     @GetMapping("/cart")
-
     public String showCart(Model model, HttpSession session) {
-        // check om attributten ‘cart’ findes i sessions objektet
-        Cart cart = (Cart) session.getAttribute("cart");
-        if (cart!= null) {
-            // hvis ikke - opret en ny indkøbskurv (cart)
-        } else {
-            // opret indkøbskurvattributten (cart) i session objektet
+
+        cart = (Cart) session.getAttribute("cart");
+        if (cart == null) {
             cart = new Cart();
             session.setAttribute("cart", cart);
-    }
-        // sæt sessionslevetiden til 30 sec til testformål
-        session.setMaxInactiveInterval(15);
 
-        // tilføj attributterne ‘items’, og ‘total’ til model objektet
-        model.addAttribute(cart.getItems());
-        model.addAttribute(cart.getTotal());
+            session.setMaxInactiveInterval(30);
 
-        // returner cart.html, der viser ui
-        return "index";
+            return "cart";
+        }
+
+        model.addAttribute("items",  cart.getItems());
+        model.addAttribute("total", cart.getTotal());
+
+        return "cart";
     }
 
     @PostMapping("/cart/add")
     public String addToCart(@RequestParam String name, @RequestParam double price, @RequestParam int quantity) {
-        // opret nyt item
-        CartItem cartItem = new CartItem(name,price,quantity);
 
-        // tilføj item til cart
+        CartItem cartItem = new CartItem(name, price, quantity);
         cart.addItem(cartItem);
 
-        // redirect til /cart
-        return "redirect: /cart";
+        return "redirect:/cart";
     }
 
     @PostMapping("/cart/remove")
-
     public String removeFromCart(@RequestParam int index) {
-        // slet item på ‘index’ i cart
         cart.removeItem(index);
-
-        // redirect til /cart
-        return "redirect: /cart";
+        return "redirect:/cart";
     }
 
-    @PostMapping("/cart/empty")
 
+    @PostMapping("/cart/tøm")
     public String emptyCart(HttpSession session) {
-        // fjern ‘cart’ attribute fra session objekt
         session.removeAttribute("cart");
-
-        // redirect til /cart
-        return "redirect: /cart";
+        return "redirect:/cart";
     }
-
 }
